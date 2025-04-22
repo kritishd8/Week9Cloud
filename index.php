28a67bea7ff0b9ea717b7fc22dc0286043b5c4c1
Obscure. <69711181+kritishd8@users.noreply.github.com>
2025-04-22 14:43:54 +0545

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Kritish Dhakal</title>
+    <title>Lorem Ipsum</title>
     <style>
         body {
             font-family: Arial, sans-serif;
